1132261aafaacf1ac35c10837a2e8e8f3bd97677
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-02-07 15:40:57 +0600

    update typescript editor setup & compolation

---
+++ 01.Editor-Setup-Compilation/output/main.js
@@ -0,0 +1,6 @@
+"use strict";
+const country = "Bangladesh";
+const text = `I love ${country}`;
+const text2 = `I don't love ${country}`;
+console.log(text);
+console.log(text2);
---
+++ 01.Editor-Setup-Compilation/ts/main.ts
@@ -0,0 +1,6 @@
+const country = "Bangladesh";
+const text = `I love ${country}`;
+const text2 = `I don't love ${country}`;
+
+console.log(text);
+console.log(text2);
